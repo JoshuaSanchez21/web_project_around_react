6cd8cfbc22f877b70656e4ee0425b5109faa163e
JoshuaSanchez <misael_san12@hotmail.com>
2026-03-06 09:28:36 -0600

    GitHub pages path arreglado

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/WEB_PROYECT_AROUND_REACT/",
   server: {
     port: 3000,
   },
